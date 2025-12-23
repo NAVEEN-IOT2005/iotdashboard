94f71d0c402817e9523a8c4a18cfc50c5af53e07
NAVEEN K <kumarb08021983@gmail.com>
2025-12-23 10:59:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const WS_URL = "wss://iot-ws-server.onrender.com";
+const socket = new WebSocket(WS_URL);
+
+socket.onopen = () => {
+  console.log("WebSocket Connected");
+};
+
+socket.onmessage = e => {
+  const data = JSON.parse(e.data);
+
+  const card = document.querySelector(
+    `.card[data-id="${data.id}"]`
+  );
+  if (!card) return;
+
+  card.classList.toggle("on", data.state);
+};
+
+document.querySelectorAll(".toggle").forEach(toggle => {
+  toggle.addEventListener("click", () => {
+    const card = toggle.closest(".card");
+    const id = card.dataset.id;
+    const state = !card.classList.contains("on");
+
+    socket.send(JSON.stringify({
+      id,
+      state
+    }));
+  });
+});
